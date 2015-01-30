44490ec9f8d3314667fece7a38527e851bb0155b
Zack Rock <zacharyrock@hotmail.com>
2015-01-29 21:53:23 -0800

    EVERYTHING

---
+++ Assets/MINE/PlayerCamera.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour {
+
+	public float speed = 2f;
+
+	public LayerMask mask;
+
+	public Shader waterShader;
+
+	void Start () {
+//		camera.RenderWithShader(waterShader
+		GetComponent<BlurEffect> ().enabled = false;
+	}
+	
+	void Update () {
+		Vector3 dir = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Lateral"), Input.GetAxis ("Vertical"));
+
+		transform.position += dir * speed * Time.deltaTime;
+
+		Vector3 point = camera.ViewportToWorldPoint (new Vector3(Screen.width / 2.0f, 0f));
+
+//		float distance = (camera.ViewportToWorldPoint (new Vector3 (Screen.width / 2.0f, Screen.height)) - point).magnitude;
+
+//		RaycastHit hit;
+//
+//		if (Physics.Raycast (new Ray (point, -transform.up), out hit, distance, mask)) {
+//			renderer.material.SetFloat("WaterHeight", hit.distance);
+//		}
+
+//		Plane[] planes = GeometryUtility.CalculateFrustumPlanes (camera);
+
+//		planes[0].
+	}
+
+	void WaterEnter() {
+		GetComponent<BlurEffect> ().enabled = true;
+	}
+
+	void WaterExit() {
+		GetComponent<BlurEffect> ().enabled = false;
+	}
+}
---
+++ Assets/MINE/SpawnOnClick.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnOnClick : MonoBehaviour {
+
+	public GameObject[] spawnables;
+
+	int curSelection = 0;
+
+	GameObject curObject;
+
+	void Start () {
+		curObject = (GameObject)Instantiate (spawnables [curSelection]);
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			curSelection--;
+			if(curSelection < 0) {
+				curSelection = spawnables.Length - 1;
+				Destroy(curObject);
+				curObject = (GameObject)Instantiate (spawnables [curSelection]);
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.E)) {
+			curSelection++;
+			if(curSelection > spawnables.Length - 1) {
+				curSelection = 0;
+				Destroy(curObject);
+				curObject = (GameObject)Instantiate (spawnables [curSelection]);
+			}
+		}
+
+		RaycastHit hit;
+		if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hit)) {
+			curObject.transform.position = hit.point;
+
+			if (Input.GetButtonDown ("Fire1")) {
+				curObject.transform.parent = hit.transform.parent;
+				curObject = (GameObject)Instantiate (spawnables [curSelection]);
+			}
+		}
+	}
+}
