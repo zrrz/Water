0ccb12b34325d78f2c29856216e0be69a1397a2c
Zack Rock <zacharyrock@hotmail.com>
2015-02-13 23:56:05 -0800

    I cant keep eyes open. Okay for now, Stall this week and we'll make it amazin next week.

---
+++ Assets/PuzzleGame/PuzzleManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuzzleManager : MonoBehaviour {
+
+	public PuzzlePiece heldPiece;
+
+	public LayerMask pieceMask;
+	public LayerMask moveMask;
+	public LayerMask placeMask;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		if(heldPiece != null) {
+			RaycastHit hit;
+			if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 200f, moveMask)) {
+				heldPiece.transform.position = hit.point;
+			}
+		}
+
+		if(Input.GetButtonDown("Fire1")) {
+			if(heldPiece != null) {
+				RaycastHit hit;
+				if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 200f, placeMask)) {
+					heldPiece.transform.parent = hit.collider.transform;
+					heldPiece.transform.localPosition = Vector3.zero;
+					heldPiece = null;
+				}
+			} else {
+				RaycastHit hit;
+				if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 200f, pieceMask)) {
+					PuzzlePiece piece = hit.collider.GetComponent<PuzzlePiece>();
+					heldPiece = piece.GetPiece();
+					heldPiece.transform.parent = null;
+					heldPiece.transform.localScale = hit.collider.transform.localScale;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/PuzzleGame/SpotGizmos.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpotGizmos : MonoBehaviour {
+	void OnDrawGizmos() {
+		for (int i = 0; i < transform.childCount; i++) {
+			Gizmos.color = Color.red;
+			Gizmos.DrawRay(transform.GetChild(i).position, Vector3.up);
+		}
+	}
+}
